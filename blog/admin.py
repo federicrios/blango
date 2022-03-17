0a9b266d987175964cff6e4d8af03989b8a0f2a8
Federico Rios <federicrios@gmail.com>
2022-03-17 22:07:56 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -8,6 +8,6 @@ class PostAdmin(admin.ModelAdmin):
       'slug', 'published_at'
     ]
 
-
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
