018d4d754b3fa50d5691c16aa19a08045efb94ae
Federico Rios <federicrios@gmail.com>
2022-03-23 12:26:29 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'crispy_forms'
+    'crispy_forms',
+    'rest_framework'
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
